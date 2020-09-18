23d29a2b009122b50f35f9492365bbec39a4c4fe
Dedaldino3D <dedaldinoantonio7@gmail.com>
2020-09-18 15:58:09 +0100

    Creating mod_inbox (XEP - 0430) for ejabberd based on MongooseIM mod_inbox

---
+++ mod_inbox_ejabberd/include/mod_inbox.hrl
@@ -0,0 +1,28 @@
+-type username() :: jid:luser().
+
+-type host() :: jid:lserver().
+
+-type sender() :: binary().
+
+-type content() :: binary().
+
+-type count_bin() :: binary().
+
+-type unread_bin() :: binary().
+
+-type active_bin() :: binary().
+
+-type name_bin() :: binary().
+
+-type id() :: binary().
+
+-type get_inbox_res() :: list(inbox_res()).
+
+-type inbox_res() :: {RemoteBinJID :: binary(),
+                      MsgContent :: content(),
+                      UnreadCount :: count_bin(),
+                      Timestamp :: erlang:timestamp()}.
+
+-type inbox_write_res() :: ok | {error, any()}.
+
+-type marker() :: binary().
