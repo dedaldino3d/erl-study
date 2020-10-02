b6a38d74e51c31a3a3638db8687f91ffa021f815
Dedaldino3D <dedaldinoantonio7@gmail.com>
2020-10-02 17:44:25 +0100

    Remote and create new hooks

--- mod_inbox_ejabberd/mod_inbox_muc.erl
+++ mod_inbox_ejabberd/mod_inbox_muc.erl
@@ -1,15 +1,20 @@
 ﻿%%%-------------------------------------------------------------------
-%%% @author Dominik Stanaszek dominik.stanaszek@erlang-solutions.com
-%%% @copyright (C) 2018, Erlang-Solutions
+%%% @author dedaldino3D
+%%% @copyright (C) 2020, dedaldino3D
 %%% @doc
 %%%
 %%% @end
 %%% Created : 6.07.2018
 %%%-------------------------------------------------------------------
 -module(mod_inbox_muc).
--author("dominik.stanaszek@erlang-solutions.com").
--include("jlib.hrl").
--include("mongoose.hrl").
+-author("dedaldino3D").
+% * Remove include
+% -include("jlib.hrl").
+% * Remove include
+% -include("mongoose.hrl").
+% * For it
+-include_lib("xmpp/include/xmpp.hrl").
+
 
 -export([update_inbox_for_muc/1, start/1, stop/1]).
 
@@ -19,7 +24,25 @@
 -type receiver_host() :: jid:lserver().
 -type receiver_bare_user_jid() :: user_jid().
 -type room_bare_jid() :: jid:jid().
--type packet() :: exml:element().
+% ? Changed exml:element()
+-type packet() :: xmlel().
+
+% ? Types from Mongoo mod_muc_room
+-type users_map() :: #{{user(), server(), resource()} => user()}.
+-type users_pairs() :: [{{user(), server(), resource()}, user()}].
+-type affiliations_map() :: [{{user(), server(), resource()}, mod_muc:affiliation()}].
+
+
+-type update_inbox_for_muc_payload() :: #{
+        room_jid := jid:jid(),
+        from_jid := jid:jid(),
+        from_room_jid := jid:jid(),
+        packet := xmlel(),
+        affiliations_map := affiliations_map()
+        }.
+
+-export_type([update_inbox_for_muc_payload/0]).
+
 
 start(Host) ->
     ejabberd_hooks:add(update_inbox_for_muc, Host, ?MODULE, update_inbox_for_muc, 90),
@@ -32,7 +55,60 @@ stop(Host) ->
     ok.
 
 
+
+% ? from mongoose_hooks
+% *** THIS IS A HOOK
+-spec update_inbox_for_muc(Server, Info) -> Result when
+    Server :: jid:server(),
+    Info :: update_inbox_for_muc_payload(),
+    Result :: update_inbox_for_muc_payload().
+update_inbox_for_muc(Server, Info) ->
+    ejabberd_hooks:run_fold(update_inbox_for_muc, Server, Info, []).
+
+
+
+% ? Types from Mongoo mod_muc_room, original
+% -spec run_update_inbox_for_muc_hook(jid:server(),
+%                                     update_inbox_for_muc_payload()) -> ok.
+% run_update_inbox_for_muc_hook(ServerHost, HookInfo) ->
+%     % * ejabberd_hooks dont have update_inbox_for_muc, create it here
+%     mongoose_hooks:update_inbox_for_muc(ServerHost, HookInfo),
+%     ok.
+
+% * Changed version
+-spec run_update_inbox_for_muc_hook(jid:server(),
+                                    update_inbox_for_muc_payload()) -> ok.
+run_update_inbox_for_muc_hook(ServerHost, HookInfo) ->
+    update_inbox_for_muc(ServerHost, HookInfo),
+    ok.
+
+
+% ! Cal; run_update_inbox_for_muc_hook inside a hook 
+% ! [muc_filter_message, ...]  find how messages in rooms
+% ! are handled
+% ! check mond_muc_room line 893 in mongoose
+
+
+
+% ? from mongoose_hooks
+% *** THIS IS A HOOK
+%%% @doc The `inbox_unread_count' hook is called to get the number of unread messages in the inbox for a user.
+-spec inbox_unread_count(LServer, Acc, User) -> Result when
+        LServer :: jid.lserver(),
+        % !Remove acumulator
+        Acc :: mongoose_acc:t(),
+        User :: jid:jid(),
+        % !Remove acumulator
+        Result :: mongoose_acc:t().
+inbox_unread_count(LServer, Acc, User) ->
+    ejabberd_hooks:run_fold(inbox_unread_count, LServer, Acc, [User]).
+
+
+
+
+% ? Remove or perform update for Acc (acumulators)
 -spec update_inbox_for_muc(Acc) -> Acc when
+    % ! Check mod_muc_room
       Acc :: mod_muc_room:update_inbox_for_muc_payload().
 update_inbox_for_muc(
     #{room_jid := Room,
@@ -43,19 +119,23 @@ update_inbox_for_muc(
     F = fun(AffLJID, Affiliation) ->
             case is_allowed_affiliation(Affiliation) of
                 true ->
+                    % ! jid:to_bare() doesnt exist in jid, remove
                     To = jid:to_bare(jid:make(AffLJID)),
                     %% Guess direction based on user JIDs
                     Direction = direction(From, To),
                     Host = To#jid.lserver,
+                    % ! jid:replace_from_to() doesnt exist in jid, remove
                     Packet2 = jlib:replace_from_to(FromRoomJid, To, Packet),
                     update_inbox_for_user(Direction, Host, Room, To, Packet2);
                 false ->
                     ok
             end
         end,
+    % ! mongoose_lib doesnt exist, remove
     mongoose_lib:maps_foreach(F, AffsMap),
     Acc.
 
+% ! Check mod_muc:affiliation()
 -spec is_allowed_affiliation(mod_muc:affiliation()) -> boolean().
 is_allowed_affiliation(outcast) -> false;
 is_allowed_affiliation(_)       -> true.
@@ -80,6 +160,7 @@ update_inbox_for_user(Direction, Host, Room, To, Packet) ->
 
 -spec direction(From :: user_jid(), To :: user_jid()) -> incoming | outgoing.
 direction(From, To) ->
+    % ? check jid:are_bare_equal()
     case jid:are_bare_equal(From, To) of
         true -> outgoing;
         false -> incoming
@@ -119,4 +200,5 @@ write_to_receiver_inbox(Server, User, Remote, Packet) ->
 %% A local host can be used to fire hooks or write into database on this node.
 -spec is_local_xmpp_host(jid:lserver()) -> boolean().
 is_local_xmpp_host(LServer) ->
+    % ! my hosts came from mongoose.hrl, remove it and use others approaches
     lists:member(LServer, ?MYHOSTS).
